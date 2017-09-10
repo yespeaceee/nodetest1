39188e074e552a1c69b8a0c743dc7be5b807db99
Damir Bogdanov <yespeaceee@gmail.com>
2017-09-10 16:55:07 +1000

    Part 3

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,20 @@ router.get('/helloworld', function(req, res) {
   res.render('helloworld', { title: 'Hello, World!' });
 });
 
+/* GET Userlist page. */
+router.get('/userlist', function(req,res){
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({},{},function(e,docs){
+    res.render('userlist', {
+      "userlist" : docs
+    });
+  });
+});
+
+/* GET New user page */
+router.get('/newuser', function(req, res){
+  res.render('newuser', { title: 'Add new user' });
+});
+
 module.exports = router;
